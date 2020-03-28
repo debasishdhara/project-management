9a0a5cdb5f292af605ae5d6aa26ea2ea01ad2b1f
debasishdhara.dhara <debasishdhara.dhara@gmail.com>
2020-03-28 21:19:40 +0530

    Changes For Demo View

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1305,8 +1305,8 @@
                     <div class="single-icon">
                       <i class="fa fa-map-marker"></i>
                       <p>
-                        Location: A108 Adam Street<br>
-                        <span>NY 535022, USA</span>
+                        Location: Demo Street<br>
+                        <span>Demo , Demo</span>
                       </p>
                     </div>
                   </div>
@@ -1317,7 +1317,9 @@
                 <!-- Start Google Map -->
                 <div class="col-md-6 col-sm-6 col-xs-12">
                   <!-- Start Map -->
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d22864.11283411948!2d-73.96468908098944!3d40.630720240038435!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew+York%2C+NY%2C+USA!5e0!3m2!1sen!2sbg!4v1540447494452" width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  <a href="javascript:void(0);">
+                    <img src="{{asset('assets/img/blog/3.jpg')}}" alt="">
+                    </a>
                   <!-- End Map -->
                 </div>
                 <!-- End Google Map -->
@@ -1327,7 +1329,7 @@
                   <div class="form contact-form">
                     <div id="sendmessage">Your message has been sent. Thank you!</div>
                     <div id="errormessage"></div>
-                    <form action="" method="post" role="form" class="contactForm">
+                    {{--  <form action="" method="post" role="form" class="contactForm">  --}}
                       <div class="form-group">
                         <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
                         <div class="validation"></div>
@@ -1345,7 +1347,7 @@
                         <div class="validation"></div>
                       </div>
                       <div class="text-center"><button type="submit">Send Message</button></div>
-                    </form>
+                    {{--  </form>  --}}
                   </div>
                 </div>
                 <!-- End Left contact -->
